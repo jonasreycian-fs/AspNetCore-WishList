1c7b3d4e45fe36f3e0e7d79e1f0b79e1cfb24db9
Jonas Reycian B. Saraosos <jsaraosos@fullscale.io>
2022-10-03 23:57:46 +0800

    Updated the controller

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -27,25 +27,21 @@ namespace WishList.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            return View("Create");
+            var model = _context.Items.ToList();
+            return View("Index", model);
         }
 
         [HttpPost]
-        public IActionResult Create([FromBody] Item item)
+        public IActionResult Create(Models.Item item)
         {
             _context.Items.Add(item);
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
 
-        [HttpDelete]
-        public IActionResult Delete(int id)
+        public IActionResult Delete(int Id)
         {
-            var itemToDelete = _context.Items.FirstOrDefault((x) => x.Id == id);
-            if (itemToDelete != null)
-            {
-                return NotFound();
-            }
+            var itemToDelete = _context.Items.FirstOrDefault((x) => x.Id == Id);
             _context.Items.Remove(itemToDelete);
             _context.SaveChanges();
             return RedirectToAction("Index");
