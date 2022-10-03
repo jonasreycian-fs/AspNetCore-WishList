af2ac0260a37bec0cc1703412a12381df084fefb
Jonas Reycian B. Saraosos <jsaraosos@fullscale.io>
2022-10-03 17:43:26 +0800

    Updated homecontroller

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -8,22 +8,13 @@ using Microsoft.Extensions.Logging;
 
 namespace WishList.Controllers
 {
-    [Route("[controller]")]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View("Index");
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View("Error");
